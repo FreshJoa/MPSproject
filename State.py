24d06b1eda3c6ef0b845f01384c98c812a564f31
Asia <mazureek6@gmail.com>
2021-06-01 17:56:22 +0200

    poprawki2

--- State.py
+++ State.py
@@ -4,9 +4,6 @@ class State:
     def __init__(self, alpha):
         self.alpha = alpha
 
-    def __call__(self, x):
-        return np.exp(self.alpha*x**2.)
-
     def GetPsi(self, x):
         return np.exp(self.alpha*x**2.)
 
